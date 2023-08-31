283f6f2b74f3c09b78305aff8b7b75b18a70f1fd
lauragarcia7585 <50127494+lauragarcia7585@users.noreply.github.com>
2023-08-31 00:41:46 -0500

    Subo avances del frontend de página de recursos compartidos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './components/recursos-compartidos/recursos-compartidos.component.html',
+  styleUrls: ['./components/recursos-compartidos/recursos-compartidos.component.css']
 })
 export class AppComponent {
   title = 'paginaWMI';
---
+++ src/app/components/recursos-compartidos/recursos-compartidos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recursos-compartidos',
+  templateUrl: './recursos-compartidos.component.html',
+  styleUrls: ['./recursos-compartidos.component.css']
+})
+export class RecursosCompartidosComponent {
+
+}
